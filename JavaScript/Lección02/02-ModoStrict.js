599f8e4b8ca44df614e2b9b367d41efd515e7c5d
Mariana Aguilera <marian5174@gmail.com>
2025-04-20 20:04:22 -0300

    Modo Strict

---
+++ JavaScript/Lección02/02-ModoStrict.js
@@ -0,0 +1,12 @@
+"use strict"; //no permite el uso de alguna varible sin declarar
+let x = 10;
+console.log(x);
+
+miFuncion();
+
+function miFuncion(){
+    //"use strict";
+   y = 10;
+   console.log(y);
+}
+miFuncion();
