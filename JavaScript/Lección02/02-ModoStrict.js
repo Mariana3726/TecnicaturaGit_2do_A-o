20cd1854215141743f8cfb0ac8a222b602ef7f87
Mariana Aguilera <marian5174@gmail.com>
2025-04-20 20:20:32 -0300

    Importancia de Quokka

--- JavaScript/Lección02/02-ModoStrict.js
+++ JavaScript/Lección02/02-ModoStrict.js
@@ -1,12 +1,15 @@
 "use strict"; //no permite el uso de alguna varible sin declarar
-let x = 10;
+x = 10;
 console.log(x);
 
 miFuncion();
 
 function miFuncion(){
-    //"use strict";
-   y = 10;
+    "use strict";
+   let y = 10;
    console.log(y);
 }
 miFuncion();
+
+//Es importante que a la hora de usa el modo strict, desactivemos y volvamos a activar quokka, 
+// para que reconozca los errores
