2de2920e4201774549a0152e0824233f473d2f0d
Mariana Aguilera <marian5174@gmail.com>
2025-04-20 02:14:37 -0300

    Métodos enter y exit

--- Python/Archivos/Lección02/pythonProject/archivos_con_with.py
+++ Python/Archivos/Lección02/pythonProject/archivos_con_with.py
@@ -1,6 +1,10 @@
-#Manejo del contexto with: sintaxis simplificada
-# La ventaja de with es que abre y cierra el archivo de forma automática,
+from ManejoArchivos import ManejoArchivos
+
+#Manejo del contexto with: sintaxis simplificada.La ventaja de with es que abre y cierra el archivo de forma automática,
 #no es necesario try ni finally, utiliza diferentes métodos: __enter__ que abre
 # __exit__ que cierra
-with open('prueba.txt', 'r', encoding='UTF-8') as archivo:
+#with open('prueba.txt', 'r', encoding='UTF-8') as archivo:
+    #print(archivo.read())
+
+with ManejoArchivos('prueba.txt') as archivo:
     print(archivo.read())
