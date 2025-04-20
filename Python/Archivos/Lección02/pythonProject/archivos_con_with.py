04263ec4978c62baddb6ef16d3ded592ca861bd2
Mariana Aguilera <marian5174@gmail.com>
2025-04-20 01:47:42 -0300

    Manejo del contexto With

---
+++ Python/Archivos/Lección02/pythonProject/archivos_con_with.py
@@ -0,0 +1,6 @@
+#Manejo del contexto with: sintaxis simplificada
+# La ventaja de with es que abre y cierra el archivo de forma automática,
+#no es necesario try ni finally, utiliza diferentes métodos: __enter__ que abre
+# __exit__ que cierra
+with open('prueba.txt', 'r', encoding='UTF-8') as archivo:
+    print(archivo.read())
