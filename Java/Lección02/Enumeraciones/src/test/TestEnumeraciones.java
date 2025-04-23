04a0c86d5aa6d351efa3284f85468f2cb4760d1a
Mariana Aguilera <marian5174@gmail.com>
2025-04-22 21:58:13 -0300

    Prueba de enumeraciones, con días de la semana

--- Java/Lección02/Enumeraciones/src/test/TestEnumeraciones.java
+++ Java/Lección02/Enumeraciones/src/test/TestEnumeraciones.java
@@ -5,5 +5,35 @@ import enumeraciones.Dias;
 public class TestEnumeraciones {
     public static void main(String[] args) {
         System.out.println("Dia 1: "+Dias.LUNES);
+        indicarDiaSemana(Dias.JUEVES); //las enumeraciones se tratan como cadenas, pero no son cadenas
+        //no se debe utilizar comillas, se accede a través del punto.
+    }
+    
+    private static void indicarDiaSemana(Dias dias){
+        switch(dias){
+            case LUNES:
+                System.out.println("Primer día de la semana");
+                break;
+            case MARTES:
+                System.out.println("Segundo día de la semana");
+                break;
+            case MIERCOLES:
+                System.out.println("Tercer día de la semana");
+                break;
+            case JUEVES:
+                System.out.println("Cuarto día de la semana");
+                break;
+            case VIERNES:
+                System.out.println("Quinto día de la semana");
+                break;
+            case SABADO:
+                System.out.println("Sexto día de la semana");
+                break;
+            case DOMINGO:
+                System.out.println("Séptimo día de la semana");
+                break;
+            default:
+                System.out.println("Día no válido");
+        }
     }
 }
