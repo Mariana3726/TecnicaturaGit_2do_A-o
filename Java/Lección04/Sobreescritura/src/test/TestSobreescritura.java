fd6cbcc055d04ad358ebc2efc3d31efff268ce5e
Mariana Aguilera <marian5174@gmail.com>
2025-05-06 23:25:00 -0300

    Polimorfismo

--- Java/Lección04/Sobreescritura/src/test/TestSobreescritura.java
+++ Java/Lección04/Sobreescritura/src/test/TestSobreescritura.java
@@ -1,12 +1,21 @@
 
 package test;
 
-import domain.Gerente;
+import domain.*;
 
 
 public class TestSobreescritura {
     public static void main(String[] args) {
-        Gerente gerente1 = new Gerente("Mariana", 5000, "Sistemas");
-        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+        Empleado empleado1 = new Empleado("Juan", 3000);
+        imprimir(empleado1);
+        //System.out.println("empleado1 = " + empleado1.obtenerDetalles());
+        empleado1 = new Gerente("Mariana", 5000, "Sistemas");
+        imprimir(empleado1);
+        //System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+    }
+    
+    public static void imprimir(Empleado empleado){
+        String detalles = empleado.obtenerDetalles();
+        System.out.println("detalles = "+ detalles);
     }
 }
