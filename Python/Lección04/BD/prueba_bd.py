90fec152ec94634c3e9750ff28518e37f40f7c2a
Mariana Aguilera <marian5174@gmail.com>
2025-05-08 22:14:42 -0300

    Función fetchone

--- Python/Lección04/BD/prueba_bd.py
+++ Python/Lección04/BD/prueba_bd.py
@@ -11,9 +11,10 @@ try: #bloque try-except-finally
 # se usa para manejar errores de forma segura y cerrar la conexión pase lo que pase
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia = 'SELECT * FROM persona'
-            cursor.execute(sentencia) #de esta manera ejecutamos la sentencia
-            registros = cursor.fetchall() #recuperamos todos los registros que serán una lista
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s' #Placeholder
+            id_persona = input('Ingrese un número para el id_persona: ')
+            cursor.execute(sentencia,(id_persona,)) #de esta manera ejecutamos la sentencia
+            registros = cursor.fetchone() #recuperamos todos los registros que serán una lista
             print(registros) #imprime los resultados en forma de tuplas
 except Exception as e: #manejo de errores, si por ej la tabla no existe, se muestra ese mensaje
     print(f'Ocurrió un error: {e}')
