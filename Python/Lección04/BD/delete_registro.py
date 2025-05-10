bff207fca4213e88b11aba64c68231c223fe141f
Mariana Aguilera <marian5174@gmail.com>
2025-05-09 22:40:16 -0300

    Eliminamos un registro con DELETE

---
+++ Python/Lección04/BD/delete_registro.py
@@ -0,0 +1,26 @@
+import psycopg2 #importación de la librería 'psycopg2' para conectarnos a Postgres
+
+conexion = psycopg2.connect( #conexión a la base de datos
+    user='postgres', #usuario
+    password='admin', #contraseña
+    host='127.0.0.1', #dirección del servidor
+    port='5432', #puerto en el que escucha postgreSQL
+    database='test_bd' #nombre de la base de datos
+)
+try: #bloque try-except-finally
+# se usa para manejar errores de forma segura y cerrar la conexión pase lo que pase
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'DELETE FROM persona WHERE id_persona=%s'
+            entrada = input('Ingrese el número de registro a eliminar: ')
+            valores = (entrada,) #es una tupla de valores
+            cursor.execute(sentencia, valores) #de esta manera ejecutamos la sentencia,executemany para varios registros
+
+            registros_eliminados = cursor.rowcount
+            print(f'Los registros eliminados son: {registros_eliminados}')
+
+
+except Exception as e: #manejo de errores, si por ej la tabla no existe, se muestra ese mensaje
+    print(f'Ocurrió un error: {e}')
+finally:
+    conexion.close() #cierra la conexión
