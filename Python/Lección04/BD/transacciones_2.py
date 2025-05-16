20b92119e0de81da2b3b3a84bbd851f9347c392c
Mariana Aguilera <marian5174@gmail.com>
2025-05-15 21:44:23 -0300

    Transacciones parte 2

---
+++ Python/Lección04/BD/transacciones_2.py
@@ -0,0 +1,28 @@
+import psycopg2 as bd
+
+conexion = bd.connect(
+    user='postgres',
+    password='admin',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+try:
+    conexion.autocommit = False #esto no debería estar, iicia la transacción
+    cursor = conexion.cursor()
+    sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+    valores = ('Marina', 'Ecarsola', 'ms123@gmail.com')
+    cursor.execute(sentencia, valores)
+
+    sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    valores = ('Juan', 'Juarez','jjuarez@gmaiñ.com', 1)
+    cursor.execute(sentencia, valores)
+
+    conexion.commit()  #hacemos el commit manualmente, se cierra la transacción
+    print('Termina la transacción')
+
+except Exception as e:
+    conexion.rollback()
+    print(f'Ocurrió un error, se hizo un rollback: {e}')
+finally:
+    conexion.close()
