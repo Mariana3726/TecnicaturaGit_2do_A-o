7648cfdc56cc47c33fba5858b816fc4499be936d
Mariana Aguilera <marian5174@gmail.com>
2025-06-08 19:50:37 -0300

    Método actualizar

--- Python/Lección06/capas_datos_persona/persona_dao.py
+++ Python/Lección06/capas_datos_persona/persona_dao.py
@@ -29,7 +29,38 @@ class PersonaDAO:
                     personas.append(persona)
                 return personas
 
+    @classmethod
+    def insertar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email)
+                cursor.execute(cls._INSERTAR, valores)
+                log.debug(f'Persona insertada: {persona}')
+                return cursor.rowcount
+    @classmethod
+    def actualizar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email, persona.id_persona)
+                cursor.execute(cls._ACTUALIZAR, valores)
+                log.debug(f'Persona actualizada: {persona}')
+                return cursor.rowcount
+
+
 if __name__== '__main__':
+    # Actualizar un registro
+    persona1 = Persona(1, 'Juan José', 'Pena', 'jjpena@gmail.com')
+    personas_actualizadas = PersonaDAO.actualizar(persona1)
+    log.debug(f'Personas actualizadas: {personas_actualizadas}')
+
+
+    # Insertar un registro
+    # persona1 = Persona(nombre='Roque', apellido='Ramos', email='roque234@gmail.com')
+    # personas_insertadas = PersonaDAO.insertar(persona1)
+    # log.debug(f'Personas insertadas: {personas_insertadas}')
+
+
+    # Seleccionar objetos
     personas = PersonaDAO.seleccionar()
     for persona in personas:
         log.debug(persona)
