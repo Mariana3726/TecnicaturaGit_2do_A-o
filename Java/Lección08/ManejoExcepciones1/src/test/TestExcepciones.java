83da5453fc86e44b76b2ebb6dced224389bbf697
Mariana Aguilera <marian5174@gmail.com>
2025-06-08 22:24:19 -0300

    Manejo de excepciones

---
+++ Java/Lección08/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -0,0 +1,17 @@
+package test;
+
+import static aritmetica.Aritmetica.division;
+
+public class TestExcepciones {
+    public static void main(String[] args) {
+        int resultado = 0;
+        try{
+            resultado = division(10, 0);
+        } catch(Exception e){
+            System.out.println("Ocurrió un error");
+            e.printStackTrace(System.out); //se conoce como pila de excepciones
+            System.out.println(e.getMessage());
+        }
+        System.out.println("La variable de resultado tiene como valor: "+ resultado);
+    }
+}
