64a2288d6b18565f98de4e32a8e56e1e0391b361
Mariana Aguilera <marian5174@gmail.com>
2025-06-14 14:42:40 -0300

    Continuamos con el menú

--- Java/Lección09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/Lección09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -5,11 +5,31 @@ public class CalculadoraUTN {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("******* Aplicación Calculadora *******");
-        System.out.print("Ingrese el valor para el operando1: ");
-        var operando1 = Integer.parseInt(entrada.nextLine());
-        System.out.print("Ingrese el valor para el operando2: ");
-        var operando2 = Integer.parseInt(entrada.nextLine());
-        var resultado = operando1 + operando2;
-        System.out.println("resultado = " + resultado);
+        //Mostramos el Menú
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicación
+                4. División
+                5. Salir
+                """);
+        System.out.print("Operación a realizar?: ");
+        var operacion = Integer.parseInt(entrada.nextLine());
+
+        if(operacion >= 1 && operacion <= 4){
+            System.out.print("Ingrese el valor para el operando1: ");
+            var operando1 = Integer.parseInt(entrada.nextLine());
+            System.out.print("Ingrese el valor para el operando2: ");
+            var operando2 = Integer.parseInt(entrada.nextLine());
+        }
     }
 }
+
+
+
+
+
+
+
+
+
