1adc66122563cbb7329624a3c5e0aab88671a5c2
Mariana Aguilera <marian5174@gmail.com>
2025-06-14 15:33:21 -0300

    Ciclo y salida con break

--- Java/Lección09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/Lección09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -4,51 +4,58 @@ public class CalculadoraUTN {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("******* Aplicación Calculadora *******");
-        //Mostramos el Menú
-        System.out.println("""
-                1. Suma
-                2. Resta
-                3. Multiplicación
-                4. División
-                5. Salir
-                """);
-        System.out.print("Operación a realizar?: ");
-        var operacion = Integer.parseInt(entrada.nextLine());
+        while (true){ //va a ser un ciclo infinito
 
-        if(operacion >= 1 && operacion <= 4){
-            System.out.print("Ingrese el valor para el operando1: ");
-            var operando1 = Integer.parseInt(entrada.nextLine());
-            System.out.print("Ingrese el valor para el operando2: ");
-            var operando2 = Integer.parseInt(entrada.nextLine());
 
-            int resultado;
-            switch (operacion){
-                case 1 -> {
-                    resultado = operando1 + operando2;
-                    System.out.println("Resultado de la suma: "+resultado);
-                }
-                case 2 -> {
-                    resultado = operando1 - operando2;
-                    System.out.println("Resultado de la resta: "+resultado);
-                }
-                case 3 -> {
-                    resultado = operando1 * operando2;
-                    System.out.println("Resultado de la multiplicación: "+resultado);
-                }
-                case 4 -> {
-                    resultado = operando1 / operando2;
-                    System.out.println("Resultado de la división: "+resultado);
-                }
-                default -> System.out.println("Opción errónea: "+operacion);
-            } //Fin switch
-        } //Fin del if
-        else if (operacion == 5) {
-            System.out.println("Fin");
-        }
-        else {
-            System.out.println("Opción errónea: "+operacion);
-        }
+            System.out.println("******* Aplicación Calculadora *******");
+            //Mostramos el Menú
+            System.out.println("""
+                    1. Suma
+                    2. Resta
+                    3. Multiplicación
+                    4. División
+                    5. Salir
+                    """);
+            System.out.print("Operación a realizar?: ");
+            var operacion = Integer.parseInt(entrada.nextLine());
+
+            if(operacion >= 1 && operacion <= 4){
+                System.out.print("Ingrese el valor para el operando1: ");
+                var operando1 = Integer.parseInt(entrada.nextLine());
+                System.out.print("Ingrese el valor para el operando2: ");
+                var operando2 = Integer.parseInt(entrada.nextLine());
+
+                int resultado;
+                switch (operacion){
+                    case 1 -> {
+                        resultado = operando1 + operando2;
+                        System.out.println("Resultado de la suma: "+resultado);
+                    }
+                    case 2 -> {
+                        resultado = operando1 - operando2;
+                        System.out.println("Resultado de la resta: "+resultado);
+                    }
+                    case 3 -> {
+                        resultado = operando1 * operando2;
+                        System.out.println("Resultado de la multiplicación: "+resultado);
+                    }
+                    case 4 -> {
+                        resultado = operando1 / operando2;
+                        System.out.println("Resultado de la división: "+resultado);
+                    }
+                    default -> System.out.println("Opción errónea: "+operacion);
+                } //Fin switch
+            } //Fin del if
+            else if (operacion == 5) {
+                System.out.println("Fin");
+                break; //rompe el ciclo y sale
+            }
+            else {
+                System.out.println("Opción errónea: "+operacion);
+            }
+            //imprimimos un salto de línea antes de repetir el menú
+            System.out.println();
+        } //fin ciclo while
     } //Fin main
 } //Fin clase
 
