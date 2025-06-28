2cc242729441a000195563c7242c4e2b7501a707
Mariana Aguilera <marian5174@gmail.com>
2025-06-28 15:55:02 -0300

    Creamos la base de datos estudiantes2025

---
+++ Java/Lección11/base_estudiantes2025.sql
@@ -0,0 +1,6 @@
+
+-- Comenzamos con CRUD: create(insertar), read(leer), update(actualizar), delete(eliminar)
+-- Listar los estudiantes (read)
+SELECT * FROM estudiantes2025;
+-- Insertar estudiante
+INSERT INTO estudiantes2025(nombre, apellido, telefono, email) VALUES ("Juan", "Perez", "2617898766", "juanperez@gmail.com")
